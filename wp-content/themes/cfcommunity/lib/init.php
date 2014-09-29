60782b8456fad98203eb6f90504fdb3cc966c0d0
Bowe Frankema <bowe@cfcommunity.net>
2014-09-29 20:40:21 +0300

    Get rid of Roots file organisation. Shit's crazy
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/themes/cfcommunity/lib/init.php
+++ wp-content/themes/cfcommunity/lib/init.php
@@ -27,15 +27,397 @@ function roots_setup() {
     'primary_navigation_es' => __('Spanish Navigation', 'roots'),
   ));
 
-  // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
   add_theme_support('post-thumbnails');
-  // set_post_thumbnail_size(150, 150, false);
-  // add_image_size('category-thumb', 300, 9999); // 300px wide (and unlimited height)
-
-  // Add post formats (http://codex.wordpress.org/Post_Formats)
-  // add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
 
   // Tell the TinyMCE editor to use a custom stylesheet
   add_editor_style('/assets/css/editor-style.css');
 }
 add_action('after_setup_theme', 'roots_setup');
+
+/**
+ * Cleaner walker for wp_nav_menu()
+ *
+ * Walker_Nav_Menu (WordPress default) example output:
+ *   <li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8"><a href="/">Home</a></li>
+ *   <li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9"><a href="/sample-page/">Sample Page</a></l
+ *
+ * Roots_Nav_Walker example output:
+ *   <li class="menu-home"><a href="/">Home</a></li>
+ *   <li class="menu-sample-page"><a href="/sample-page/">Sample Page</a></li>
+ */
+class Roots_Nav_Walker extends Walker_Nav_Menu {
+  function check_current($classes) {
+    return preg_match('/(current[-_])|active|dropdown/', $classes);
+  }
+
+  function start_lvl(&$output, $depth = 0, $args = array()) {
+    $output .= "\n<ul class=\"dropdown-menu\">\n";
+  }
+
+  function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+    $item_html = '';
+    parent::start_el($item_html, $item, $depth, $args);
+
+    if ($item->is_dropdown && ($depth === 0)) {
+      $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $item_html);
+      $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
+    }
+    elseif (stristr($item_html, 'li class="divider')) {
+      $item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);
+    }
+    elseif (stristr($item_html, 'li class="dropdown-header')) {
+      $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
+    }
+
+    $item_html = apply_filters('roots_wp_nav_menu_item', $item_html);
+    $output .= $item_html;
+  }
+
+  function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
+    $element->is_dropdown = ((!empty($children_elements[$element->ID]) && (($depth + 1) < $max_depth || ($max_depth === 0))));
+
+    if ($element->is_dropdown) {
+      $element->classes[] = 'dropdown';
+    }
+
+    parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+  }
+}
+
+/**
+ * Remove the id="" on nav menu items
+ * Return 'menu-slug' for nav menu classes
+ */
+function roots_nav_menu_css_class($classes, $item) {
+  $slug = sanitize_title($item->title);
+  $classes = preg_replace('/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes);
+  $classes = preg_replace('/^((menu|page)[-_\w+]+)+/', '', $classes);
+
+  $classes[] = 'menu-' . $slug;
+
+  $classes = array_unique($classes);
+
+  return array_filter($classes, 'is_element_empty');
+}
+add_filter('nav_menu_css_class', 'roots_nav_menu_css_class', 10, 2);
+add_filter('nav_menu_item_id', '__return_null');
+
+/**
+ * Clean up wp_nav_menu_args
+ *
+ * Remove the container
+ * Use Roots_Nav_Walker() by default
+ */
+function roots_nav_menu_args($args = '') {
+  $roots_nav_menu_args['container'] = false;
+
+  if (!$args['items_wrap']) {
+    $roots_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
+  }
+
+  if (current_theme_supports('bootstrap-top-navbar') && !$args['depth']) {
+    $roots_nav_menu_args['depth'] = 2;
+  }
+
+  if (!$args['walker']) {
+    $roots_nav_menu_args['walker'] = new Roots_Nav_Walker();
+  }
+
+  return array_merge($args, $roots_nav_menu_args);
+}
+add_filter('wp_nav_menu_args', 'roots_nav_menu_args');
+
+/**
+ * Add Bootstrap thumbnail styling to images with captions
+ * Use <figure> and <figcaption>
+ *
+ * @link http://justintadlock.com/archives/2011/07/01/captions-in-wordpress
+ */
+function roots_caption($output, $attr, $content) {
+  if (is_feed()) {
+    return $output;
+  }
+
+  $defaults = array(
+    'id'      => '',
+    'align'   => 'alignnone',
+    'width'   => '',
+    'caption' => ''
+  );
+
+  $attr = shortcode_atts($defaults, $attr);
+
+  // If the width is less than 1 or there is no caption, return the content wrapped between the [caption] tags
+  if ($attr['width'] < 1 || empty($attr['caption'])) {
+    return $content;
+  }
+
+  // Set up the attributes for the caption <figure>
+  $attributes  = (!empty($attr['id']) ? ' id="' . esc_attr($attr['id']) . '"' : '' );
+  $attributes .= ' class="thumbnail wp-caption ' . esc_attr($attr['align']) . '"';
+  $attributes .= ' style="width: ' . (esc_attr($attr['width']) + 10) . 'px"';
+
+  $output  = '<figure' . $attributes .'>';
+  $output .= do_shortcode($content);
+  $output .= '<figcaption class="caption wp-caption-text">' . $attr['caption'] . '</figcaption>';
+  $output .= '</figure>';
+
+  return $output;
+}
+add_filter('img_caption_shortcode', 'roots_caption', 10, 3);
+
+
+/**
+ * Clean up the_excerpt()
+ */
+function roots_excerpt_length($length) {
+  return POST_EXCERPT_LENGTH;
+}
+
+function roots_excerpt_more($more) {
+  return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'roots') . '</a>';
+}
+add_filter('excerpt_length', 'roots_excerpt_length');
+add_filter('excerpt_more', 'roots_excerpt_more');
+
+
+
+/**
+ * Redirects search results from /?s=query to /search/query/, converts %20 to +
+ *
+ * @link http://txfx.net/wordpress-plugins/nice-search/
+ */
+function roots_nice_search_redirect() {
+  global $wp_rewrite;
+  if (!isset($wp_rewrite) || !is_object($wp_rewrite) || !$wp_rewrite->using_permalinks()) {
+    return;
+  }
+
+  $search_base = $wp_rewrite->search_base;
+  if (is_search() && !is_admin() && strpos($_SERVER['REQUEST_URI'], "/{$search_base}/") === false) {
+    wp_redirect(home_url("/{$search_base}/" . urlencode(get_query_var('s'))));
+    exit();
+  }
+}
+if (current_theme_supports('nice-search')) {
+  add_action('template_redirect', 'roots_nice_search_redirect');
+}
+
+/**
+ * Fix for empty search queries redirecting to home page
+ *
+ * @link http://wordpress.org/support/topic/blank-search-sends-you-to-the-homepage#post-1772565
+ * @link http://core.trac.wordpress.org/ticket/11330
+ */
+function roots_request_filter($query_vars) {
+  if (isset($_GET['s']) && empty($_GET['s'])) {
+    $query_vars['s'] = ' ';
+  }
+
+  return $query_vars;
+}
+add_filter('request', 'roots_request_filter');
+
+/**
+ * Tell WordPress to use searchform.php from the templates/ directory
+ */
+function roots_get_search_form($form) {
+  $form = '';
+  locate_template('/templates/searchform.php', true, false);
+  return $form;
+}
+add_filter('get_search_form', 'roots_get_search_form');
+
+/**
+ * Use Bootstrap's media object for listing comments
+ *
+ * @link http://getbootstrap.com/components/#media
+ */
+class Roots_Walker_Comment extends Walker_Comment {
+  function start_lvl(&$output, $depth = 0, $args = array()) {
+    $GLOBALS['comment_depth'] = $depth + 1; ?>
+    <ul <?php comment_class('media list-unstyled comment-' . get_comment_ID()); ?>>
+    <?php
+  }
+
+  function end_lvl(&$output, $depth = 0, $args = array()) {
+    $GLOBALS['comment_depth'] = $depth + 1;
+    echo '</ul>';
+  }
+
+  function start_el(&$output, $comment, $depth = 0, $args = array(), $id = 0) {
+    $depth++;
+    $GLOBALS['comment_depth'] = $depth;
+    $GLOBALS['comment'] = $comment;
+
+    if (!empty($args['callback'])) {
+      call_user_func($args['callback'], $comment, $args, $depth);
+      return;
+    }
+
+    extract($args, EXTR_SKIP); ?>
+
+  <li id="comment-<?php comment_ID(); ?>" <?php comment_class('media comment-' . get_comment_ID()); ?>>
+    <?php include(locate_template('templates/comment.php')); ?>
+  <?php
+  }
+
+  function end_el(&$output, $comment, $depth = 0, $args = array()) {
+    if (!empty($args['end-callback'])) {
+      call_user_func($args['end-callback'], $comment, $args, $depth);
+      return;
+    }
+    echo "</div></li>\n";
+  }
+}
+
+function roots_get_avatar($avatar, $type) {
+  if (!is_object($type)) { return $avatar; }
+
+  $avatar = str_replace("class='avatar", "class='avatar pull-left media-object", $avatar);
+  return $avatar;
+}
+add_filter('get_avatar', 'roots_get_avatar', 10, 2);
+
+/**
+ * Determines whether or not to display the sidebar based on an array of conditional tags or page templates.
+ *
+ * If any of the is_* conditional tags or is_page_template(template_file) checks return true, the sidebar will NOT be displayed.
+ *
+ * @link http://roots.io/the-roots-sidebar/
+ *
+ * @param array list of conditional tags (http://codex.wordpress.org/Conditional_Tags)
+ * @param array list of page templates. These will be checked via is_page_template()
+ *
+ * @return boolean True will display the sidebar, False will not
+ */
+class Roots_Sidebar {
+  private $conditionals;
+  private $templates;
+
+  public $display = true;
+
+  function __construct($conditionals = array(), $templates = array()) {
+    $this->conditionals = $conditionals;
+    $this->templates    = $templates;
+
+    $conditionals = array_map(array($this, 'check_conditional_tag'), $this->conditionals);
+    $templates    = array_map(array($this, 'check_page_template'), $this->templates);
+
+    if (in_array(true, $conditionals) || in_array(true, $templates)) {
+      $this->display = false;
+    }
+  }
+
+  private function check_conditional_tag($conditional_tag) {
+    if (is_array($conditional_tag)) {
+      return $conditional_tag[0]($conditional_tag[1]);
+    } else {
+      return $conditional_tag();
+    }
+  }
+
+  private function check_page_template($page_template) {
+    return is_page_template($page_template);
+  }
+}
+
+/**
+ * Page titles
+ */
+function roots_title() {
+  if (is_home()) {
+    if (get_option('page_for_posts', true)) {
+      return get_the_title(get_option('page_for_posts', true));
+    } else {
+      return __('Latest Posts', 'roots');
+    }
+  } elseif (is_archive()) {
+    $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+    if ($term) {
+      return apply_filters('single_term_title', $term->name);
+    } elseif (is_post_type_archive()) {
+      return apply_filters('the_title', get_queried_object()->labels->name);
+    } elseif (is_day()) {
+      return sprintf(__('Daily Archives: %s', 'roots'), get_the_date());
+    } elseif (is_month()) {
+      return sprintf(__('Monthly Archives: %s', 'roots'), get_the_date('F Y'));
+    } elseif (is_year()) {
+      return sprintf(__('Yearly Archives: %s', 'roots'), get_the_date('Y'));
+    } elseif (is_author()) {
+      $author = get_queried_object();
+      return sprintf(__('Author Archives: %s', 'roots'), $author->display_name);
+    } else {
+      return single_cat_title('', false);
+    }
+  } elseif (is_search()) {
+    return sprintf(__('Search Results for %s', 'roots'), get_search_query());
+  } elseif (is_404()) {
+    return __('Not Found', 'roots');
+  } else {
+    return get_the_title();
+  }
+}
+
+/**
+ * Utility functions
+ */
+function add_filters($tags, $function) {
+  foreach($tags as $tag) {
+    add_filter($tag, $function);
+  }
+}
+
+function is_element_empty($element) {
+  $element = trim($element);
+  return empty($element) ? false : true;
+}
+
+/**
+ * Theme wrapper
+ *
+ * @link http://roots.io/an-introduction-to-the-roots-theme-wrapper/
+ * @link http://scribu.net/wordpress/theme-wrappers.html
+ */
+function roots_template_path() {
+  return Roots_Wrapping::$main_template;
+}
+
+function roots_sidebar_path() {
+  return new Roots_Wrapping('templates/sidebar.php');
+}
+
+class Roots_Wrapping {
+  // Stores the full path to the main template file
+  static $main_template;
+
+  // Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
+  static $base;
+
+  public function __construct($template = 'base.php') {
+    $this->slug = basename($template, '.php');
+    $this->templates = array($template);
+
+    if (self::$base) {
+      $str = substr($template, 0, -4);
+      array_unshift($this->templates, sprintf($str . '-%s.php', self::$base));
+    }
+  }
+
+  public function __toString() {
+    $this->templates = apply_filters('roots_wrap_' . $this->slug, $this->templates);
+    return locate_template($this->templates);
+  }
+
+  static function wrap($main) {
+    self::$main_template = $main;
+    self::$base = basename(self::$main_template, '.php');
+
+    if (self::$base === 'index') {
+      self::$base = false;
+    }
+
+    return new Roots_Wrapping();
+  }
+}
+add_filter('template_include', array('Roots_Wrapping', 'wrap'), 99);
