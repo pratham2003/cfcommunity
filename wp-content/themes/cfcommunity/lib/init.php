c8eb6dc544032e30ef2977e25a4be6006e925c8d
Bowe Frankema <bowe@cfcommunity.net>
2014-09-29 20:48:51 +0300

    Further clean-up of the theme
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/themes/cfcommunity/lib/init.php
+++ wp-content/themes/cfcommunity/lib/init.php
@@ -1,5 +1,85 @@
 <?php
 /**
+ * Enable theme features
+ */
+add_theme_support('root-relative-urls');    // Enable relative URLs
+add_theme_support('bootstrap-top-navbar');  // Enable Bootstrap's top navbar
+add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails component on [gallery]
+add_theme_support('nice-search');           // Enable /?s= to /search/ redirect
+add_theme_support('jquery-cdn');            // Enable to load jQuery from the Google CDN
+
+/**
+ * Configuration values
+ */
+define('GOOGLE_ANALYTICS_ID', ''); // UA-XXXXX-Y (Note: Universal Analytics only, not Classic Analytics)
+define('POST_EXCERPT_LENGTH', 40); // Length in words for excerpt_length filter (http://codex.wordpress.org/Plugin_API/Filter_Reference/excerpt_length)
+
+/**
+ * .main classes
+ */
+function roots_main_class() {
+  if (roots_display_sidebar()) {
+    // Classes on pages with the sidebar
+    $class = 'col-sm-8';
+  } else {
+    // Classes on full width pages
+    $class = 'col-sm-12';
+  }
+
+  return $class;
+}
+
+/**
+ * .sidebar classes
+ */
+function roots_sidebar_class() {
+  return 'col-sm-4';
+}
+
+/**
+ * Define which pages shouldn't have the sidebar
+ *
+ * See lib/sidebar.php for more details
+ */
+function roots_display_sidebar() {
+  $sidebar_config = new Roots_Sidebar(
+    /**
+     * Conditional tag checks (http://codex.wordpress.org/Conditional_Tags)
+     * Any of these conditional tags that return true won't show the sidebar
+     *
+     * To use a function that accepts arguments, use the following format:
+     *
+     * array('function_name', array('arg1', 'arg2'))
+     *
+     * The second element must be an array even if there's only 1 argument.
+     */
+    array(
+      'is_404',
+      'is_front_page'
+    ),
+    /**
+     * Page template checks (via is_page_template())
+     * Any of these page templates that return true won't show the sidebar
+     */
+    array(
+      'template-custom.php'
+    )
+  );
+
+  return apply_filters('roots_display_sidebar', $sidebar_config->display);
+}
+
+/**
+ * $content_width is a global variable used by WordPress for max image upload sizes
+ * and media embeds (in pixels).
+ *
+ * Example: If the content area is 640px wide, set $content_width = 620; so images and videos will not overflow.
+ * Default: 1140px is the default Bootstrap container width.
+ */
+if (!isset($content_width)) { $content_width = 1140; }
+
+
+/**
  * Roots initial setup and constants
  */
 function roots_setup() {
@@ -358,6 +438,137 @@ function roots_title() {
     return get_the_title();
   }
 }
+
+/**
+ * Clean up gallery_shortcode()
+ *
+ * Re-create the [gallery] shortcode and use thumbnails styling from Bootstrap
+ * The number of columns must be a factor of 12.
+ *
+ * @link http://getbootstrap.com/components/#thumbnails
+ */
+function roots_gallery($attr) {
+  $post = get_post();
+
+  static $instance = 0;
+  $instance++;
+
+  if (!empty($attr['ids'])) {
+    if (empty($attr['orderby'])) {
+      $attr['orderby'] = 'post__in';
+    }
+    $attr['include'] = $attr['ids'];
+  }
+
+  $output = apply_filters('post_gallery', '', $attr);
+
+  if ($output != '') {
+    return $output;
+  }
+
+  if (isset($attr['orderby'])) {
+    $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+    if (!$attr['orderby']) {
+      unset($attr['orderby']);
+    }
+  }
+
+  extract(shortcode_atts(array(
+    'order'      => 'ASC',
+    'orderby'    => 'menu_order ID',
+    'id'         => $post->ID,
+    'itemtag'    => '',
+    'icontag'    => '',
+    'captiontag' => '',
+    'columns'    => 4,
+    'size'       => 'thumbnail',
+    'include'    => '',
+    'exclude'    => '',
+    'link'       => ''
+  ), $attr));
+
+  $id = intval($id);
+  $columns = (12 % $columns == 0) ? $columns: 4;
+  $grid = sprintf('col-sm-%1$s col-lg-%1$s', 12/$columns);
+
+  if ($order === 'RAND') {
+    $orderby = 'none';
+  }
+
+  if (!empty($include)) {
+    $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+    $attachments = array();
+    foreach ($_attachments as $key => $val) {
+      $attachments[$val->ID] = $_attachments[$key];
+    }
+  } elseif (!empty($exclude)) {
+    $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+  } else {
+    $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+  }
+
+  if (empty($attachments)) {
+    return '';
+  }
+
+  if (is_feed()) {
+    $output = "\n";
+    foreach ($attachments as $att_id => $attachment) {
+      $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+    }
+    return $output;
+  }
+
+  $unique = (get_query_var('page')) ? $instance . '-p' . get_query_var('page'): $instance;
+  $output = '<div class="gallery gallery-' . $id . '-' . $unique . '">';
+
+  $i = 0;
+  foreach ($attachments as $id => $attachment) {
+    switch($link) {
+      case 'file':
+        $image = wp_get_attachment_link($id, $size, false, false);
+        break;
+      case 'none':
+        $image = wp_get_attachment_image($id, $size, false, array('class' => 'thumbnail img-thumbnail'));
+        break;
+      default:
+        $image = wp_get_attachment_link($id, $size, true, false);
+        break;
+    }
+    $output .= ($i % $columns == 0) ? '<div class="row gallery-row">': '';
+    $output .= '<div class="' . $grid .'">' . $image;
+
+    if (trim($attachment->post_excerpt)) {
+      $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
+    }
+
+    $output .= '</div>';
+    $i++;
+    $output .= ($i % $columns == 0) ? '</div>' : '';
+  }
+
+  $output .= ($i % $columns != 0 ) ? '</div>' : '';
+  $output .= '</div>';
+
+  return $output;
+}
+if (current_theme_supports('bootstrap-gallery')) {
+  remove_shortcode('gallery');
+  add_shortcode('gallery', 'roots_gallery');
+  add_filter('use_default_gallery_style', '__return_null');
+}
+
+/**
+ * Add class="thumbnail img-thumbnail" to attachment items
+ */
+function roots_attachment_link_class($html) {
+  $postid = get_the_ID();
+  $html = str_replace('<a', '<a class="thumbnail img-thumbnail"', $html);
+  return $html;
+}
+add_filter('wp_get_attachment_link', 'roots_attachment_link_class', 10, 1);
+
 
 /**
  * Utility functions
