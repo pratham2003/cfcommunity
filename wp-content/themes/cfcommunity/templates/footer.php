8ff0ae207b8b612f5ea808a3af04ef2ec9568e04
Bowe Frankema <bowe@cfcommunity.net>
2014-11-18 22:39:51 +0100

    Update footer
    Remove Translation request and add Sponsors + WordPress <3
    
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/themes/cfcommunity/templates/footer.php
+++ wp-content/themes/cfcommunity/templates/footer.php
@@ -10,7 +10,7 @@
 				</h4>
 				<p class="text-center">
 					With CFCommunity we are creating an international community for all people affected by Cystic Fibrosis, and we're completely dependent on donations. 
-					<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> Support us!</a>
+					<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> Be awesome, Support us!</a>
 				</p>
 			</div>
 			<div class="col-sm-3">
@@ -27,25 +27,25 @@
 			</div>
 			<div class="col-sm-3">
 				<div class="text-center">
-					<span class="fa fa-globe icon-round icon-md"></span>
+					<span class="fa fa-life-ring icon-round icon-md"></span>
 				</div>
 				<h4 class="text-center ">
-					<span>Help us Translate</span>
+					<span>We Love Our Supporters</span>
 				</h4>
 				<p class="text-center">
-					We're always looking for people who want to help us translate the site into different languages. Want to join our awesome group of translation heroes?
-					<a href="#"><i class="fa fa-arrow-circle-right"></i> Go to the translation group</a>
+					Where would we be without them? We could not be more grateful to those who are making CFCommunity possible with their donations and sponsorships.
+					<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> See our Heroes</a>
 				</p>
 			</div>
 			<div class="col-sm-3">
 				<div class="text-center">
-					<span class="fa fa-globe icon-round icon-md"></span>
+					<span class="fa fa-link icon-round icon-md"></span>
 				</div>
 				<h4 class="text-center">
 					<span>We're So Social!</span>
 				</h4>
 				<p class="text-center">
-					As much as we love hanging out here, you can also find us at other places. Go ahead and follow, like and share our community with your bestest online buddies!
+					As much as we love hanging out here, you can also find us at other places. Connect with us by clicking on one of those pretty icons below!
 					<div class="social-links">
 						<a target="_blank" href="https://www.facebook.com/pages/CFCommunitynet-Where-people-with-Cystic-Fibrosis-meet/176854133478"><i class="fa fa-facebook-square fa social-fb"></i></a>
 						<a target="_blank" href="twitter.com/cfcommunity_net"><i class="fa fa-twitter-square fa social-tw"></i></a>
@@ -55,7 +55,7 @@
 			</div>
 		</div>
 		<div class="col-sm-12 bottom-links text-center">
-			Made with lots of <span class="fa fa-heart"></span> by the CFCommunity
+			Powered by <a href="http://wordpress.org"><i class="fa fa-wordpress"></i>ordPress</a></span> and made with lots of <span class="fa fa-heart"></span> by the CFCommunity<span>
 		</div>
 </footer>
 
