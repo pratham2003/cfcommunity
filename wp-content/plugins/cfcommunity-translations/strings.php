4b20b3e3c3020014ee0c25c4aac789f78c5bc351
Bowe Frankema <bowe@cfcommunity.net>
2014-11-18 15:44:34 +0100

    Update translation strings
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/plugins/cfcommunity-translations/strings.php
+++ wp-content/plugins/cfcommunity-translations/strings.php
@@ -1,4 +1,3 @@
-<!-- lib/buddypress/bp-actions.php -->
 <?php _e('Hi! Welcome to our Member Directory! You can use our awesome search options to quickly find people in similar situations as you. Click on the "Show Search" buttons to see all the available search options! Have fun and make some new friends!', 'cfcommunity'); ?>	
 <?php _e('Start searching for people by clicking on a search category below', 'cfcommunity'); ?>	
 <?php _e('PS: We have a zero tolerance policy regarding misuse of our search functionality. Using our search feature to contact our members for commercial/fundraising or any unwanted messages, would make us very sad. Please read our community guidelines carefully or get in touch if you have requests/questions!', 'cfcommunity'); ?>	
@@ -11,3 +10,23 @@
 <?php _e('Log In', 'cfcommunity'); ?>
 <?php _e('More about', 'cfcommunity'); ?>
 <?php _e('Group Navigation', 'cfcommunity'); ?>
+
+<!-- activity tour -->
+<?php __('Hi there! I have not seen you before! If you click the "Next" button I will give you a quick tour of the CFCommunity homepage. Go ahead :-)', 'cfcommunity'); ?>
+
+<?php __('Welcome to CFCommunity', 'cfcommunity'); ?>	
+
+<?php __('Notifications', 'cfcommunity'); ?>	
+<?php __('Sweet! So when something happens on CFCommunity that deserves your attention you will get a notification.', 'cfcommunity'); ?>	
+<?php __('Your Profile Menu', 'cfcommunity'); ?>	
+<?php __('You can also quickly navigate to any profile page by using this menu. For instance to read your messages or quickly browse to one of your discussion groups.', 'cfcommunity'); ?>	
+<?php __('The latest news', 'cfcommunity'); ?>	
+<?php __('The news stream shows you the activity from your friends on CFCommunity. It might be a bit empty since you might not not made any friends yet. Poor you!', 'cfcommunity'); ?>	
+<?php __('News Filters', 'cfcommunity'); ?>	
+<?php __('Luckily there is a solution for that! Simply use the filters to quickly change the activity that is being displayed. Try it by clicking on the "All Members" button now!', 'cfcommunity'); ?>	
+<?php __('Show your interests', 'cfcommunity'); ?>	
+<?php __('You might just want to see certain types of activity. For example you might just want to see Blog posts from your friends. You can use the dropdown to select any type of activity you would like to see and your stream will be instantly updated!', 'cfcommunity'); ?>	
+<?php __('Write your first update!', 'cfcommunity'); ?>	
+<?php __('You can write an update to your friends on CFCommunity by typing something in this box.Go ahead, write your first message!', 'cfcommunity'); ?>	
+<?php __('Publish away!', 'cfcommunity'); ?>	
+<?php __('This is the end of my tour! Go ahead and press the "Post Update" button now. I hope you like CFCommunity and you might see me monkeying around on other pages on CFCommunity to show you around! <br><br><strong>Click on "End the Tour" to continue</strong>', 'cfcommunity'); ?>	
