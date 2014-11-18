3ed09a46e82cc839bb16604048e65b56f24c25cb
Bowe Frankema <bowe@cfcommunity.net>
2014-11-18 19:14:55 +0100

    Update footer!
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/themes/cfcommunity/templates/footer.php
+++ wp-content/themes/cfcommunity/templates/footer.php
@@ -5,46 +5,57 @@
 				<div class="text-center">
 					<a href="index.html"><span class="fa fa-heart icon-round icon-md"></span></a>
 				</div>
-				<h3 class="text-center ">
-					Become a Supporter
-				</h3>
+				<h4 class="text-center ">
+					<span>Support our Cause</span>
+				</h4>
 				<p class="text-center">
-					A little feature description could go here. A little feature description.
+					With CFCommunity we are creating an international community for all people affected by Cystic Fibrosis, and we're completely dependent on donations. 
+					<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> Support us!</a>
 				</p>
 			</div>
 			<div class="col-sm-3">
 				<div class="text-center">
 					<span class="fa fa-group icon-round icon-md"></span>
 				</div>
-				<h3 class="text-center ">
-					About the Project
-				</h3>
+				<h4 class="text-center ">
+					<span>Meet the Team</span>
+				</h4>
 				<p class="text-center">
-					A little feature description could go here. A little feature description.
+					Without these lovely people CFCommunity would be nowhere. Come check out their pretty faces and even get to know them a little!
+					<a href="http://cfcommunity.net/team/"><i class="fa fa-arrow-circle-right"></i> Check out the Team!</a>
 				</p>
 			</div>
 			<div class="col-sm-3">
 				<div class="text-center">
 					<span class="fa fa-globe icon-round icon-md"></span>
 				</div>
-				<h3 class="text-center ">
-					Help us Translate
-				</h3>
+				<h4 class="text-center ">
+					<span>Help us Translate</span>
+				</h4>
 				<p class="text-center">
-					A little feature description could go here. A little feature description.
+					We're always looking for people who want to help us translate the site into different languages. Want to join our awesome group of translation heroes?
+					<a href="#"><i class="fa fa-arrow-circle-right"></i> Go to the translation group</a>
 				</p>
 			</div>
 			<div class="col-sm-3">
 				<div class="text-center">
-					<span class="fa fa-wechat icon-round icon-md"></span>
+					<span class="fa fa-globe icon-round icon-md"></span>
 				</div>
-				<h3 class="text-center ">
-					Spread the Word
-				</h3>
+				<h4 class="text-center">
+					<span>We're So Social!</span>
+				</h4>
 				<p class="text-center">
-					A little feature description could go here. A little feature description.
+					As much as we love hanging out here, you can also find us at other places. Go ahead and follow, like and share our community with your bestest online buddies!
+					<div class="social-links">
+						<a target="_blank" href="https://www.facebook.com/pages/CFCommunitynet-Where-people-with-Cystic-Fibrosis-meet/176854133478"><i class="fa fa-facebook-square fa social-fb"></i></a>
+						<a target="_blank" href="twitter.com/cfcommunity_net"><i class="fa fa-twitter-square fa social-tw"></i></a>
+						<a target="_blank" href="http://cfcommunity.net/contact"><i id="social" class="fa fa-envelope-square"></i></a>
+					</div>
 				</p>
 			</div>
+		</div>
+		<div class="col-sm-12 bottom-links text-center">
+			Made with lots of <span class="fa fa-heart"></span> by the CFCommunity
 		</div>
 </footer>
 
