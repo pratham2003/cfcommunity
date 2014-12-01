974fd086b56e78a7cf0fcdb9160abf5faee5c15f
Bowe Frankema <bowe@cfcommunity.net>
2014-12-01 17:20:59 +0100

    Do a proper check for user language instead of site language
    Based menu on user language not site language. props @Mamaduka
    
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/themes/cfcommunity/templates/header-navigation.php
+++ wp-content/themes/cfcommunity/templates/header-navigation.php
@@ -1,10 +1,14 @@
 <?php
- $currentlang = get_bloginfo('language');
+global $current_user;
+get_currentuserinfo(); // wordpress global variable to fetch logged in user info
+
+$userID = $current_user->ID; // logged in user's ID
+$currentlang = get_user_meta($userID, 'user_language', true);
 ?>
 
 <nav class="collapse navbar-collapse" role="navigation">
 
-<?php if ( $currentlang=="en-US" || !$currentlang=="nl-NL" ): ?>
+<?php if ( $currentlang=="en_US" || !$currentlang=="nl_NL" ): ?>
   <?php
     if (has_nav_menu('primary_navigation')) :
       wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
@@ -13,7 +17,7 @@
 
   <?php endif; ?>
 
-<?php if($currentlang=="nl-NL"): ?>
+<?php if($currentlang=="nl_NL"): ?>
  
   <?php
     if (has_nav_menu('primary_navigation_nl')) :
