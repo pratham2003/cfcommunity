242fc8b76126f3529791b0c25a3b7f0d2965f73e
Bowe Frankema <bowe@cfcommunity.net>
2014-12-12 20:15:19 +0100

    Updated Styling
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/mu-plugins/cfcommunity-cleanup.php
+++ wp-content/mu-plugins/cfcommunity-cleanup.php
@@ -9,6 +9,27 @@ Description: Clean up some stuff!
 if ( ! is_main_site() ) {
   add_filter( 'wpseo_use_page_analysis', '__return_false' );
   add_filter( 'wpseo_use_page_analysis', '__return_false' );
+
+  // stop wp removing div tags
+  function cfc_tinymce_fix( $init ) 
+  {
+      // html elements being stripped
+      $init['extended_valid_elements'] = 'div[*],article[*]';
+
+      // don't remove line breaks
+      $init['remove_linebreaks'] = false; 
+
+      // convert newline characters to BR
+      $init['convert_newlines_to_brs'] = true; 
+
+      // don't remove redundant BR
+      $init['remove_redundant_brs'] = false;
+
+      // pass back to wordpress
+      return $init;
+  }
+  add_filter('tiny_mce_before_init', 'cfc_tinymce_fix');
+
 }
 
 //Remove version numbers
