615c311d457dfbe906cb541fbf0cff3301ab62b6
George Mamadashvili <georgemamadashvili@gmail.com>
2014-12-18 17:06:20 +0400

    Fix unexpected end of file errors
    * Don't use PHP shorttags.
    * Cleans up sidebar.php template.

--- wp-content/themes/cfcommunity/templates/sidebar.php
+++ wp-content/themes/cfcommunity/templates/sidebar.php
@@ -1,27 +1,20 @@
+<?php
+/**
+ * Sidebar containing the main widget area.
+ */
 
-  <?php
-    if ( function_exists( 'bp_is_member' ) && !bp_is_my_profile() ) {?>
-      <div class="widget bp-user-navigation-widget">
-		<?php
-			get_template_part( 'buddypress/parts/bp-member-nav' );
-		?>
+if ( function_exists( 'bp_is_member' ) && ! bp_is_my_profile() ) :
+?>
+	<div class="widget bp-user-navigation-widget">
+		<?php get_template_part( 'buddypress/parts/bp-member-nav' ); ?>
 		<div style="clear:both;"></div>
-       </div>
+    </div>
+<?php endif; ?>
 
-    <?}
-  ?>
+<?php do_action( 'open_sidebar' ); ?>
 
+<?php  cfc_base_sidebars(); ?>
 
+<?php do_action( 'close_sidebar' ); ?>
 
-		<?php
-			do_action( 'open_sidebar' );?>
-
-		<?php
-			// Load Sidebars
-			cfc_base_sidebars();
-			do_action( 'close_sidebar' );
-		?>
-
-	<?php
-		do_action( 'after_sidebar' );
-	?>
+<?php do_action( 'after_sidebar' ); ?>
