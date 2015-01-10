08b0cda103e27971050174890b8cc527199470fa
Bowe Frankema <bowe@cfcommunity.net>
2015-01-10 16:30:52 +0100

    Starting point for Donate Page
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

---
+++ wp-content/themes/cfcommunity-support/front-page.php
@@ -0,0 +1,62 @@
+<div class="jumbotron no-margin-top">
+
+  <div class="container">
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <h2 class="section-title page-top">
+        <span><?php _e('Your gift will help those affected by Cystic Fibrosis', 'cfcommunity'); ?></span>
+        </h2>
+      </div>
+
+      <div class="col-sm-6 col-md-6 col-lg-6 col-xs-12">
+        <p class="lead">
+          <span>
+            <?php _e('CFCommunity is an online meeting place created by people with CF, for people with CF.', 'cfcommunity'); ?>   
+
+            <br><br>
+            <?php _e('We want to make it easy for those who live or work with CF everyday to connect.', 'cfcommunity'); ?>   
+
+            <br><br>
+            <?php _e('Learn more about us or...', 'cfcommunity'); ?>  </p>
+            <a href="<?php echo bp_get_signup_page()?>" class="btn-block btn btn-success" type="button"><i class="fa fa-user"></i> <?php _e('Sign up for CFCommunity', 'cfcommunity'); ?> </a>
+          </span>
+        </p>
+      </div>
+
+      <div class="col-sm-6 col-md-6 col-lg-6 col-xs-12">
+        <div class="container-video ">
+          <a href="https://www.youtube.com/watch?v=7gtdpnKbT10" target="_self" class="litebox">
+            <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/video.png" alt="cc-license" title="cc-license" />  
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+<div class="container">
+  <div class="content row row-offcanvas row-offcanvas-left">
+    <div class="main col-xs-12 col-sm-12" role="main">
+
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h2 class="section-title grey"><?php _e('What role does Cystic Fibrosis play in your life?', 'cfcommunity'); ?></h2>
+        </div>
+      </div>
+
+      <div class="row margin-top-20">
+
+
+      </div>
+    </div>
+  </div>
+
+
+
+</div><!-- /.main -->
+</div><!-- /.content -->
+</div>
