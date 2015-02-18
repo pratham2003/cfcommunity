e5e495e460c72cdf44b5e4bc522b2ef787e3b647
Bowe Frankema <bowe@cfcommunity.net>
2015-02-18 12:49:35 +0100

    Fix footer html
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- wp-content/themes/cfcommunity/templates/footer.php
+++ wp-content/themes/cfcommunity/templates/footer.php
@@ -1,61 +1,68 @@
 <footer class="content-info" role="contentinfo">
-<div class="container">
-		<div class="row">
-			<div class="col-sm-3">
-				<div class="text-center">
-					<span class="fa fa-heart icon-round icon-md"></span>
-				</div>
-				<h4 class="text-center ">
-					<span><?php _e('Support our Cause', 'cfctranslation'); ?>	</span>
-				</h4>
-				<p class="text-center">
-					<?php _e('With CFCommunity we are creating an international community for all people affected by Cystic Fibrosis, and we are completely dependent on donations.', 'cfctranslation'); ?>	 
-					<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> <?php _e('Be awesome, Support us!', 'cfctranslation'); ?></a>
-				</p>
-			</div>
-			<div class="col-sm-3">
-				<div class="text-center">
-					<span class="fa fa-group icon-round icon-md"></span>
-				</div>
-				<h4 class="text-center ">
-					<span><?php _e('Meet the Team', 'cfctranslation'); ?>	</span>
-				</h4>
-				<p class="text-center">
-					<?php _e('Without these lovely people CFCommunity would be nowhere. Come check out their pretty faces and even get to know them a little!', 'cfctranslation'); ?>	
-					<a href="http://cfcommunity.net/team/"><i class="fa fa-arrow-circle-right"></i> <?php _e('Check out the Team!', 'cfctranslation'); ?>	</a>
-				</p>
-			</div>
-			<div class="col-sm-3">
-				<div class="text-center">
-					<span class="fa fa-life-ring icon-round icon-md"></span>
-				</div>
-				<h4 class="text-center ">
-					<span><?php _e('We Love Our Supporters', 'cfctranslation'); ?>	</span>
-				</h4>
-				<p class="text-center">
-					<?php _e('Where would we be without them? We could not be more grateful to those who are making CFCommunity possible with their donations and sponsorships.', 'cfctranslation'); ?>	
-					<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> <?php _e('See our Heroes', 'cfctranslation'); ?>	</a>
-				</p>
-			</div>
-			<div class="col-sm-3">
-				<div class="text-center">
-					<span class="fa fa-link icon-round icon-md"></span>
-				</div>
-				<h4 class="text-center">
-					<span><?php _e('We are so social!', 'cfctranslation'); ?>	</span>
-				</h4>
-				<p class="text-center">
-					<?php _e('As much as we love hanging out here, you can also find us at other places. Connect with us by clicking on one of those pretty icons below!', 'cfctranslation'); ?>	
-					<div class="social-links">
-						<a target="_blank" href="https://www.facebook.com/pages/CFCommunitynet-Where-people-with-Cystic-Fibrosis-meet/176854133478"><i class="fa fa-facebook-square fa social-fb"></i></a>
-						<a target="_blank" href="twitter.com/cfcommunity_net"><i class="fa fa-twitter-square fa social-tw"></i></a>
-						<a target="_blank" href="http://cfcommunity.net/contact"><i id="social" class="fa fa-envelope-square"></i></a>
+	<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="text-center">
+						<span class="fa fa-heart icon-round icon-md"></span>
 					</div>
-				</p>
+					<h4 class="text-center ">
+						<span><?php _e('Support our Cause', 'cfctranslation'); ?>	</span>
+					</h4>
+					<p class="text-center">
+						<?php _e('With CFCommunity we are creating an international community for all people affected by Cystic Fibrosis, and we are completely dependent on donations.', 'cfctranslation'); ?>	 
+						<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> <?php _e('Be awesome, Support us!', 'cfctranslation'); ?></a>
+					</p>
+				</div>
+
+				<div class="col-sm-3">
+					<div class="text-center">
+						<span class="fa fa-group icon-round icon-md"></span>
+					</div>
+					<h4 class="text-center ">
+						<span><?php _e('Meet the Team', 'cfctranslation'); ?>	</span>
+					</h4>
+					<p class="text-center">
+						<?php _e('Without these lovely people CFCommunity would be nowhere. Come check out their pretty faces and even get to know them a little!', 'cfctranslation'); ?>	
+						<a href="http://cfcommunity.net/team/"><i class="fa fa-arrow-circle-right"></i> <?php _e('Check out the Team!', 'cfctranslation'); ?>	</a>
+					</p>
+				</div>
+
+				<div class="col-sm-3">
+					<div class="text-center">
+						<span class="fa fa-life-ring icon-round icon-md"></span>
+					</div>
+					<h4 class="text-center ">
+						<span><?php _e('We Love Our Supporters', 'cfctranslation'); ?>	</span>
+					</h4>
+					<p class="text-center">
+						<?php _e('Where would we be without them? We could not be more grateful to those who are making CFCommunity possible with their donations and sponsorships.', 'cfctranslation'); ?>	
+						<a href="http://cfcommunity.net/support-us/"><i class="fa fa-arrow-circle-right"></i> <?php _e('See our Heroes', 'cfctranslation'); ?>	</a>
+					</p>
+				</div>
+
+				<div class="col-sm-3">
+					<div class="text-center">
+						<span class="fa fa-link icon-round icon-md"></span>
+					</div>
+					<h4 class="text-center">
+						<span><?php _e('We are so social!', 'cfctranslation'); ?>	</span>
+					</h4>
+					<p class="text-center">
+						<?php _e('As much as we love hanging out here, you can also find us at other places. Connect with us by clicking on one of those pretty icons below!', 'cfctranslation'); ?>	
+						<div class="social-links">
+							<a target="_blank" href="https://www.facebook.com/pages/CFCommunitynet-Where-people-with-Cystic-Fibrosis-meet/176854133478"><i class="fa fa-facebook-square fa social-fb"></i></a>
+							<a target="_blank" href="twitter.com/cfcommunity_net"><i class="fa fa-twitter-square fa social-tw"></i></a>
+							<a target="_blank" href="http://cfcommunity.net/contact"><i id="social" class="fa fa-envelope-square"></i></a>
+						</div>
+					</p>
+				</div>
+				</div>
+
+			<div class="col-sm-12 bottom-links text-center">
+				<?php printf( __( "Powered by %s and made with lots of <span class='fa fa-heart'></span> by the CFCommunity",'cfctranslation' ), '<a href="http://wordpress.org"><i class="fa fa-wordpress"></i>ordPress</a>' );?>
 			</div>
-		</div>
-		<div class="col-sm-12 bottom-links text-center">
-			<?php printf( __( "Powered by %s and made with lots of <span class='fa fa-heart'></span> by the CFCommunity<span>",'cfctranslation' ), '<a href="http://wordpress.org"><span><i class="fa fa-wordpress"></i>ordPress</a></span>' );?>
-		</div>
+
+	</div>
+	
 </footer>
 <?php wp_footer(); ?>
